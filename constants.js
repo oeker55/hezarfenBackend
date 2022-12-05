9cd041856a3a182c3e55d947a6769e88ae0f8829
Özgür Eker <oeker@gmail.com>
2022-12-05 17:02:30 +0300

    canstant API URL changed

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
-const API_URL = "http://localhost:5000"
+//const API_URL = "http://localhost:5000"
 //export const API_URL = "https://hezarfenbook.herokuapp.com";
+export const API_URL = "https://hezarfenbook.onrender.com";
 
 
 module.exports = {API_URL}
